6633c57e2ff63ae3864c299ab4160d79057b4f89
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-12-24 14:15:26 +0900

    Remove sub directory gem

--- ruboty-mpush/lib/ruboty/mpush.rb
+++
@@ -1,8 +0,0 @@
-require "ruboty/mpush/version"
-require "ruboty/handlers/mpush"
-
-module Ruboty
-  module Mpush
-    # Your code goes here...
-  end
-end
--- ruboty-mpush/lib/ruboty/mpush/version.rb
+++
@@ -1,5 +0,0 @@
-module Ruboty
-  module Mpush
-    VERSION = "0.0.1"
-  end
-end
